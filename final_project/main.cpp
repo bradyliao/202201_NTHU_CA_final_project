7b088832e8f4281bbcd159f22596bdba8fab0b96
bradyliao <bradyliao123@gmail.com>
2022-01-04 21:17:33 +0800

    Initial Commit

---
+++ final_project/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  final_project
+//
+//  Created by Brady Liao on 1/4/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
